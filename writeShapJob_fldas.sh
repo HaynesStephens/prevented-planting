9c0d88112869936fd48975248a9730e2ee0ecb9a
HaynesStephens <hfstephens13@gmail.com>
2023-08-18 16:59:04 -0700

    more jobs

--- writeShapJob_fldas.sh
+++ writeShapJob_fldas.sh
@@ -2,9 +2,9 @@
 
 template="#!/bin/bash
 
-#SBATCH --job-name=ShapJob_fldas_\$NUM
-#SBATCH --output=ShapJob_fldas_\$NUM.out
-#SBATCH --error=ShapJob_fldas_\$NUM.err
+#SBATCH --job-name=ShapJob_fldas_\$YEAR
+#SBATCH --output=ShapJob_fldas_\$YEAR.out
+#SBATCH --error=ShapJob_fldas_\$YEAR.err
 #SBATCH --ntasks=1
 #SBATCH --partition=broadwl
 #SBATCH --cpus-per-task=1
@@ -14,13 +14,17 @@ template="#!/bin/bash
 #SBATCH --mail-user=haynes13@uchicago.edu
 
 source activate /home/haynes13/.conda/envs/prevplant
-python /home/haynes13/code/prevented-planting/getSHAP_fldas.py \$NUM
+python /home/haynes13/code/prevented-planting/getSHAP_fldas.py \$YEAR
 "
 
 years=(1996 2006 2016)
 
 for year in "${years[@]}"; do
-    script="script_${year}.sh"
+    script="ShapJob_fldas_${year}.sh"
     echo -e "$template" | sed "s/\$YEAR/$year/g" > "$script"
     chmod +x "$script"
+    
+    # Execute the generated script
+    "./$script"
 done
+
